4f935907ff570bd0a22718d312c4a7e065e6e592
fauzan fatih assofwan <fatihassofwan02@gmail.com>
2025-03-05 08:49:54 +0700

    Add files via upload

---
+++ fungsiku.php
@@ -0,0 +1,16 @@
+<?php
+function hitung_niai($nilai_uts, $nilai_uas, $nilai_tugas) {
+    define("NILAI_UTS", 0.25);
+    define("NILAI_UAS", 0.30);
+    define("NILAI_TUGAS", 0.45);
+    $nilai_akhir = $nilai_uts * NILAI_UTS + $nilai_uas * NILAI_UAS + $nilai_tugas * NILAI_TUGAS;
+    return $nilai_akhir;
+}
+
+function kelulusan($nilai){
+ define("NILAI_MINIMAL",60);
+    
+}
+
+?>
+
